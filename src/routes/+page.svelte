939ea2f4cef2ed1cf15f6b3e895853466080f446
❯ ~ 𝗨𝗧𝗛𝗠𝗔𝗡.𝗦𝗛 ⚡   技 <149274873+codetesla51@users.noreply.github.com>
2025-09-07 20:55:43 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -161,7 +161,7 @@
     bottom: 0;
     left: 0;
     width: 100%;
-    height: 20vh;
+    height: 15vh;
     background: linear-gradient(
       to top, 
       rgba(var(--sec-rgb, 15, 15, 15), 0.95), 
