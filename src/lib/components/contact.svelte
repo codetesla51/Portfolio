44b4ccdab35a7e8ea66c8be1fea86b7818b5909b
❯ ~ 𝗨𝗧𝗛𝗠𝗔𝗡.𝗦𝗛 ⚡   技 <149274873+codetesla51@users.noreply.github.com>
2025-09-25 13:49:01 +0100

    Update contact.svelte

--- src/lib/components/contact.svelte
+++ src/lib/components/contact.svelte
@@ -40,7 +40,7 @@ import Heading from '$lib/components/heading.svelte';
         message
       });
       
-      const response = await fetch('  https://portfolio-backend-x9in.vercel.app/contacts', {
+      const response = await fetch('https://uthmangobackend.leapcell.app/contacts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
